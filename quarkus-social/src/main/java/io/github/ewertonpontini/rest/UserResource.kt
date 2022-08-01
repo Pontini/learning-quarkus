3062c7b5e6da72cbe4bfdb1e7c34ca8d5b611988
ewerton-pontini <ewerton.pontini@picpay.com>
2022-08-01 08:20:38 -0300

    refactor domain

--- quarkus-social/src/main/java/io/github/ewertonpontini/rest/UserResource.kt
+++ quarkus-social/src/main/java/io/github/ewertonpontini/rest/UserResource.kt
@@ -16,16 +16,39 @@ class UserResource {
     @POST
     @Transactional
     fun createUser(data: CreateUserRequest): Response {
-        val user = User()
-        user.name = data.name
-        user.age = data.age
+        val user = User(name = data.name, age = data.age)
         user.persist()
         return Response.ok(data).build()
     }
 
     @GET
     fun getListUsers(): Response {
-        val result = UserRepository().getAll()?.list<User>()
+        val result = UserRepository().findAll().list<User>()
         return Response.ok(result).build()
+    }
+
+
+    @DELETE
+    @Transactional
+    @Path("{id}")
+    fun deleteUser(@PathParam("id") id: Long): Response {
+        val user = UserRepository().findById(id)
+        if (user != null) {
+            user.delete()
+            return Response.ok().build()
+        }
+        return Response.status(Response.Status.NOT_FOUND).build()
+    }
+
+    @PUT
+    @Path("{id}")
+    @Transactional
+    fun updateUser(@PathParam("id") id: Long, data: CreateUserRequest): Response {
+        val user = UserRepository().findById(id)
+        if (user != null) {
+            val newUser= user.copy(id = user.id, age = data.age, name = data.name)
+            return Response.ok(newUser).build()
+        }
+        return Response.status(Response.Status.NOT_FOUND).build()
     }
 }
--- quarkus-social/src/main/java/io/github/ewertonpontini/rest/domain/User.kt
+++ quarkus-social/src/main/java/io/github/ewertonpontini/rest/domain/User.kt
@@ -1,34 +1,25 @@
 package io.github.ewertonpontini.rest.domain
 
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase
-import io.quarkus.hibernate.orm.panache.PanacheQuery
 import io.quarkus.hibernate.orm.panache.PanacheRepository
 import javax.enterprise.context.ApplicationScoped
 import javax.persistence.*
 
 @Entity
 @Table(name = "users")
-class User : PanacheEntityBase() {
-    companion object {
-        fun getAll(): MutableList<User>? {
-            return findAll<User>().list()
-        }
-    }
+data class User(
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        val id: Long? = null,
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var id: Long? = null
+        @Column(name = "name")
+        val name: String? = null,
 
-    @Column(name = "name")
-    var name: String? = null
+        @Column(name = "age")
+        val age: Int? = null,
+) : PanacheEntityBase()
 
-    @Column(name = "age")
-    var age: Int? = null
-
-}
 @ApplicationScoped()
-class UserRepository: PanacheRepository<User>{
-    fun getAll(): PanacheQuery<User>? {
-        return findAll()
-    }
+class UserRepository : PanacheRepository<User> {
+
 }
